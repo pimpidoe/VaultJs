d485bcbb123d0dcb59fdf4711802f797eb317592
Pim <79192@rocteraa-student.nl>
2018-02-22 10:36:51 +0100

    some small changes

--- js/main.js
+++ js/main.js
@@ -7,41 +7,36 @@
 // }
 var code = document.getElementById('code')
 var ButtonDisabler = document.getElementsByClassName('button')
-var button = document.getElementByClass('button')
+var CorrectCode = "213"
 
 function getNumber(button)
 {
   code.innerHTML += button.value;
-  var CodeLengthChecker = code.innerHTML.lenght;
+  var CodeLengthChecker = code.innerHTML.length;
   var DisableCounter = 0;
 
   for (DisableCounter = 0; DisableCounter < ButtonDisabler.length; DisableCounter++)
   {
-    if (CodeLengthChecker == 3) {
+  //  console.log(codeLengthChecker);
+    if (CodeLengthChecker === 3) {
       ButtonDisabler[DisableCounter].disabled = true;
     }
     else {
       ButtonDisabler[DisableCounter].disabled = false;
     }
   }
-  if (codeContainer.innerHTML == CorrectCode)
+  if (code.innerHTML === CorrectCode)
       {
         outcome.classList.add("fade-in");
         outcome.innerHTML = "THE CODE IS CORRECT"
         CorrectTimes++;
         greenLight.innerHTML = "CORRECT: " + CorrectTimes.toString() + " TIMES"
-        var audio = new Audio('snd/Open.mp3');
-        audio.play();
-        setTimeout(reset, 6000)
       }
-  else if ((codeContainer.innerHTML != CorrectCode) && (codeLengthChecker == 3) )
+  else if ((code.innerHTML != CorrectCode) && (CodeLengthChecker === 3) )
       {
         outcome.classList.add("fade-in");
         outcome.innerHTML = "THE CODE IS INCORRECT"
         IncorrectTimes++;
         redLight.innerHTML = "INCORRECT: " + IncorrectTimes.toString() + " TIMES"
-        var audio = new Audio('snd/Close.mp3');
-        audio.play();
-        setTimeout(reset, 6000)
       }
 }
